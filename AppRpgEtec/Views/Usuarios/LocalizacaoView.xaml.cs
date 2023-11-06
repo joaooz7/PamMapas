85fb4d1745e702b4ff0d971d0b6ce43ff1af3e11
Your Name <you@example.com>
2023-11-06 11:58:49 -0300

    Erro na localização

---
+++ AppRpgEtec/Views/Usuarios/LocalizacaoView.xaml.cs
@@ -0,0 +1,19 @@
+using AndroidX.Lifecycle;
+using AppRpgEtec.ViewModels.Usuarios;
+
+namespace AppRpgEtec.Views.Usuarios;
+
+public partial class LocalizacaoView : ContentPage
+{
+	LocalizacaoViewModel viewModel;
+
+	public LocalizacaoView()
+	{
+		InitializeComponent();
+
+		viewModel = new LocalizacaoViewModel();
+		BindingContext = viewModel;
+		//viewModel.InicializarMapa();
+		viewModel.ExibirUsuariosNoMapa();
+	}
+}
